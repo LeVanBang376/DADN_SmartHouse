fb93eed94eae39731bc30ce67238e25cc9025c75
NguyenManhThuyen <109166256+NguyenManhThuyen@users.noreply.github.com>
2023-03-17 22:52:16 +0700

    initial commit

---
+++ frontend/screen/settingScreen.js
@@ -0,0 +1,14 @@
+import { setStatusBarBackgroundColor } from "expo-status-bar";
+import * as React from "react";
+import { Text,View,Button,StyleSheet } from "react-native";
+
+
+const settingScreen = ({navigation}) => {
+    return (
+        <View>
+            <Text>Cài đặt</Text>
+        </View>
+    )
+}
+
+export default settingScreen
